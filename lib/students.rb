f8004675f4d45af19d2913f3610384c6085f0b16
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-03-19 16:53:36 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -14,7 +14,7 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  "SELECT SUM(tardies) AS total_tardies FROM students;"
+  "SELECT SUM(tardies) as total_tardies FROM students;"
   
 end
 
